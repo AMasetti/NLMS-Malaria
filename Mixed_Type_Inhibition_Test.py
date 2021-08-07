a932bae2a13ffcfcd09f39eb0b89c3654f34864e
AMasetti <32473909+AMasetti@users.noreply.github.com>
2021-08-07 09:57:41 -0300

    Hyperbolic Mixed-type Inhibition Model Test and README.md

---
+++ Mixed_Type_Inhibition_Test.py
@@ -0,0 +1,53 @@
+# Vamos a arrancar cargando los parametros iniciales que estimamos
+Vmax = 30
+Ks1 = 200
+Ks2 = 100
+Alpha = 0.5
+Beta = 0.3
+
+start = np.array([Vmax, Ks1, Ks2, Alpha, Beta])
+
+import scipy.optimize as optimization  # Scipy: Necesitamos esta libreria para hacer el Metodo de Minimos Cuadrados
+import numpy as np                     # Numpy: complementa a la anterior y nos deja usar matrices onda Matlab 
+import csv                             # Esta nos deja usar archivos CSV
+
+# Ahora vamos a agarrar el archivo que yo compile con tus datos que se llama 'data.csv' donde CSV es Comma Separated Values
+# y lo vamos a convertir en tres matrices de numpy
+
+A = []
+B = []
+V = []
+
+with open('data.csv') as f:
+    csv_reader = csv.reader(f, delimiter=',')
+    
+    # Evitamos la primer linea (A, B, V)
+    next(csv_reader)
+    
+    for row in csv_reader:
+        A.append(float(row[0]))
+        B.append(float(row[1]))
+        V.append(float(row[2]))
+
+# Convertimos los arrays de Python a arrays de Numpy optimizados
+A = np.array(A)
+B = np.array(B)
+V = np.array(V)
+
+# Vamos a definir nuestra funcion a fittear
+
+def f(var_indep, Vmax, Ks1, Ks2, Alpha, Beta):
+    A, B = var_indep
+    num = Vmax * ( (A/Ks1) + ( (Beta*A*B) / (Alpha*Ks1*Ks2) ) )
+    denom = 1 + (A/Ks1) + (B/Ks2) +( (A*B) / (Alpha*Ks1*Ks2))
+    return num/denom
+
+popt, pcov = optimization.curve_fit(f, (A, B), V, start)
+
+std_dev = np.sqrt(np.diag(pcov))
+
+print(f'Vmax:  {popt[0]} ± {std_dev[0]}')
+print(f'Ks1:   {popt[1]} ± {std_dev[1]}')
+print(f'Ks2:   {popt[2]} ± {std_dev[2]}')
+print(f'Alpha: {popt[3]} ± {std_dev[3]}')
+print(f'Beta:  {popt[4]} ± {std_dev[4]}')
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # NLMS-Malaria
-Repository of fitting algorithms for nonlinear functions related to Malaria Treatment 
+
+### Intro
+Repository of fitting algorithms for nonlinear functions related to Malaria Treatment.
+
+
+Concentration data from tests of two drugs A and B, and the respective velocity of action from drug A  compiled into a CSV File.
+This repo uses Scipy's `curve-fit` function to fit the model parameters to the data using Non-Linear Mean Squares.
+
+### First Test: Hyperbolic Mixed-type Inhibition Model
+![Hyperbolic Mixed-type Inhibition Model](imgs/Modelo.jpg?raw=true "Hyperbolic Mixed-type Inhibition Model")
+
+The original test involves the Hyperbolic Mixed-type Inhibition Model calculating the parameters for:
+ - Vmax
+ - Alpha
+ - Beta
+ - Ks1
+ - Ks2
+
